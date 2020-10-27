66d2cb4d8f7d0bad35e074d08425ba997f44d85b
Matomaral <48994062+Matomaral@users.noreply.github.com>
2020-10-26 21:13:00 -0500

    Add files via upload

---
+++ config/dev.js
@@ -0,0 +1,3 @@
+module.exports = {
+    posgresqlURI: 'postgres://dtyfnedpuqtglt:31ba2b80d1617b5429d2a24759154b5dc7ca198bdef49711001d08bc74c801d5@ec2-52-7-15-198.compute-1.amazonaws.com:5432/d2jlrr8mbnhl11',
+  };
